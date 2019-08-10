65e1ade58672522aa7eca4ac13be3be85cda9bad
Euiyeon-Kim <40987310+Euiyeon-Kim@users.noreply.github.com>
2019-08-10 12:48:58 +0900

    Update Path_Planning.py

--- Path_Planning.py
+++ Path_Planning.py
@@ -5,21 +5,25 @@
 - target point에 따른 path를 반환해주는 코드.
 
 ## INPUT & OUTPUT
-- input: 미션 번호 & (combine으로부터 loacl target점과 map을 입력받음)
-- output: 제어에 넘겨줄 path의 list
+- Input: Combine에서 받은 target, map
+- Output: 제어에 넘길 Path
 
 '''
-
 from Combine import Combine
 from hybrid_astar import hybrid_a_star
 
 class Path_Planning:  # Mission으로부터 mission number를 받아 그에 맞는 제어에 넘겨줄 list를 반환해줌.
     def __init__(self, mission_number):  # 초기화
         combine = Combine(mission_number)
-        _local_target = combine.get_target()
-        _map = combine.get_map()
+        self.__local_target = combine.target
+        self.__map = combine.map
+        self.__path = [(0,0)]
 
-    def get_path(self):  # 제어팀에게 넘겨줄 path point list를 생성하는 코드
-        path = [(0,0)]
-        #  _local_target과 _map을 사용하여 path를 생성함
-        return path
+    def make_path(self):
+        # Path 만들기 짜주세요ㅠ
+
+    @property
+    def path(self):
+        return self.__path
+     
+
